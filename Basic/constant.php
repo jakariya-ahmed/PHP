1b854aed072247dc69b4213060cfb0479517f102
jek9356 <devjakariya9356@gmail.com>
2025-11-12 22:57:17 +0600

    completed constant

---
+++ Basic/constant.php
@@ -0,0 +1,17 @@
+<?php
+/** Constant 
+ * 1. A constant is like a variable
+ * 2. It's value can't be changed 
+ * 3. It doesn't start with $ sign
+ * 4. It's globally accessible
+ * 5. It's name declare by uppercase
+ */
+
+// Example Cases:  1. SITE_URL   2. API_KEY   3. THEME_VERSION   5. SECRET_KEY
+
+define("SITE_NAME", "PHP Basic Learning");
+define("API_KEY","3848484844JRJR0939");
+define("API_URL", "https://api.example.com");
+define("MAX_LOGIN_ATTEMPTS", 3);
+
+echo MAX_LOGIN_ATTEMPTS;
