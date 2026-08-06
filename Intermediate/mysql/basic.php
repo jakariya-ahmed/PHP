ffd4e897d9c5090bd690bee250441625e8cebe24
jek9356 <devjakariya9356@gmail.com>
2026-08-06 08:00:40 +0600

    db functions practise

--- Intermediate/mysql/basic.php
+++ Intermediate/mysql/basic.php
@@ -27,6 +27,71 @@ AND priority IN ('High', 'Urgent');
 
 
 
+/**----------------------------  Phase 3 —Functions ----------------------------   */
+
+// Syntax: 
+SELECT FUNCTION_NAME(column_name) FROM table_name WHERE condition;
+
+SELECT CONCAT('first_name', ' ', 'last_name') AS full_name, LOWER(email) AS clean_email,
+ROUND(salary / 12, 2) AS monthly_pay FROM employees;
+
+01; 
+//-- WRONG: SELECT tries to output 100 rows, but SUM() returns 1 row!
+SELECT name, SUM(salary) FROM employees;
+//-- CORRECT: Use aggregate functions alone OR group by the individual column (learned in Phase 4)
+SELECT SUM(salary) FROM employees;
+
+
+//-- DANGEROUS: If 20 users do not have a phone number, COUNT(phone) will be 80 while COUNT(*) is 100!
+SELECT COUNT(phone) FROM users;
+//-- CORRECT: Use COUNT(*) if you want total row count regardless of NULL values
+SELECT COUNT(*) FROM users;
+
+
+//-- WRONG: If last_name is NULL, full_name becomes NULL!
+SELECT CONCAT(first_name, ' ', last_name) FROM users;
+//-- CORRECT: Use COALESCE to provide a fall-back value for NULLs
+SELECT CONCAT(first_name, ' ', COALESCE(last_name, '')) FROM users;
+
+//-- 3. Query Financial Summary
+SELECT COUNT(*) AS total_invoices, ROUND(SUM(amount * (1 + tax_rate)), 2) AS total_revenue,
+ROUND(AVG((amount), 2)) AS average_invoice_amount, 
+MIN(amount) AS min_invoice,
+MAX(amount) AS max_invoice FROM invoices;
+
+
+
+SELECT COUNT(*) AS total_orders, 
+ROUND(SUM(amount (1 + tax_rate)), 2) AS total_revenue,
+ROUND(AVG(amount), 2) AS average_order,
+MIN() AS min_order,
+MAX() AS max_order FROM orders;
+
+
+/**----------------------------  Phase 4 — Grouping ----------------------------   */
+// Syntax:
+SELECT column_name, AGGREGATE_FUNCTION(another_column)
+FROM table_name WHERE condition GROUP BY column_name
+HAVING group_condition ORDER BY AGGREGATE_FUNCTION(another_column) DESC;
+
+
+// Query Executed:
+SELECT department, SUM(amount) AS total_sales, FROM sales 
+GROUP BY department
+HAVING SUM(amount) > 400;
+
+SELECT department, SUM(amount) AS total_sales, FROM sales
+GROUP BY department HAVING SUM(amount) > 400;
+
+
+
+
+
+
+
+
+
+
 
 
 
